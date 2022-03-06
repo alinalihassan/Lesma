23288acfc2701481d8ba8a7aeb731f43d1d8d42a
Alin Ali Hassan <hassanalinali@gmail.com>
2022-03-06 18:08:29 +0100

    docs: Updated repo

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,14 +23,12 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/alinalihassan/Lesma/tree/main/docs/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/alinalihassan/Lesma/tree/main/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
