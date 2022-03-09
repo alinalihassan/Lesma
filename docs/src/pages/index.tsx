460256937e16df80f0c0d54aba0a87e84cb925f2
Alin Ali Hassan <hassanalinali@gmail.com>
2022-03-09 12:11:17 +0100

    docs: Removed unfinished parts

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        {/*<HomepageFeatures />*/}
       </main>
     </Layout>
   );
