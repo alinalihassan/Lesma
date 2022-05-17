ccd6b456b87e1559a04fd601a3bb75e8cf08bd11
Alin Ali Hassan <hassanalinali@gmail.com>
2022-05-17 14:01:29 +0200

    ci: Updated aot compilation for MacOS

--- src/liblesma/Backend/Codegen.cpp
+++ src/liblesma/Backend/Codegen.cpp
@@ -179,7 +179,9 @@ void Codegen::LinkObjectFile(const std::string &obj_filename) {
         args.push_back(obj.c_str());
     args.push_back("-o");
     args.push_back(output.c_str());
-    args.push_back("-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -lSystem");
+    args.push_back("-L");
+    args.push_back("/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib");
+    args.push_back("-lSystem");
 
     auto DiagOpts = new clang::DiagnosticOptions();
     auto DiagID = new clang::DiagnosticIDs();
