9dda15ea5b51400c5244a4b47d320c55abcddabb
Alin Ali Hassan <hassanalinali@gmail.com>
2022-08-31 02:02:30 +0300

    fix: Fixed tests error

--- scripts/run_compiler_tests.sh
+++ scripts/run_compiler_tests.sh
@@ -7,12 +7,12 @@ success_count=0
 
 # We currently cannot test compiler errors, since we assume all runs need an exit code > 0
 # We assume compiler errors throw error codes < 100 for now
-for file in "$SCRIPT_DIR"/tests/lesma/*.les; do
+for file in "$SCRIPT_DIR"/../tests/lesma/*.les; do
 	name=$(basename -s .les "${file}")
 	echo "Testing ${name}"
-	"$SCRIPT_DIR"/build/lesma run "${file}" &> /tmp/compile_output.log
+	"$SCRIPT_DIR"/../build/lesma run "${file}" &> /tmp/compile_output.log
   test_jit_ret_value=$?
-	"$SCRIPT_DIR"/build/lesma compile "${file}" &> /tmp/compile_output.log
+	"$SCRIPT_DIR"/../build/lesma compile "${file}" &> /tmp/compile_output.log
 	test_aot_ret_value=$?
   test_expected_ret_value=$(head -n 1 "$file" | cut -b 6-)
   if [ "$test_jit_ret_value" -ne "$test_expected_ret_value" ]; then
