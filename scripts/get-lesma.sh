2be44400dc7403709a72754a7db9528744264fa4
Alin Ali Hassan <hassanalinali@gmail.com>
2022-08-31 01:42:34 +0300

    fix: Updated installer to add to path and provide feedback

--- scripts/get-lesma.sh
+++ scripts/get-lesma.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+ask() {
+  while true; do
+    read -rp "$1 [Y/n] " answer
+    case $(echo "$answer" | tr "[A-Z]" "[a-z]") in
+      y|yes|"" ) return 0;;
+      n|no     ) return 1;;
+    esac
+  done
+}
+
+# Exit if any command fails
+set -e
+set -o pipefail
+
 # Check which OS the user has
 case $(uname | tr '[:upper:]' '[:lower:]') in
   linux*)
@@ -23,6 +39,7 @@ if [[ "$OS_NAME" == windows || "$OS_NAME" == notset ]]; then
 fi
 
 # Download latest asset for the platform
+echo "Downloading the latest Lesma release"
 curl -s https://api.github.com/repos/alinalihassan/Lesma/releases/latest \
 | grep $FILE_NAME \
 | cut -d : -f 2,3 \
@@ -30,10 +47,34 @@ curl -s https://api.github.com/repos/alinalihassan/Lesma/releases/latest \
 | wget -qi -
 
 # Create Directory
+echo "Creating directory at ~/.lesma"
 mkdir -p "$HOME/.lesma"
 
 # Untar in .lesma folder
+echo "Unzipped binary and standard library files"
 for z in Lesma-*.tar.gz; do tar -xf $z --strip-components=1 -C "$HOME/.lesma"; done
 
 # Remove tmp files
 for z in Lesma-*.tar.gz; do rm $z; done
+
+# Add directory to PATH
+if [[ ":$PATH:" == *":$HOME/.lesma/bin:"* ]]; then
+  # Directory already in PATH
+  true
+else
+  if ask "Would you like to add Lesma to PATH?" >/dev/null; then
+    if [ -f "$HOME/.bashrc" ]; then
+      echo "export PATH=\$PATH:\$HOME/.lesma/bin" >> "$HOME/.bashrc"
+      echo "Added Lesma to PATH in .bashrc, please run 'source ~/.bashrc' to reload shell"
+    fi
+
+    if [ -f "$HOME/.zshrc" ]; then
+      echo "export PATH=\$PATH:\$HOME/.lesma/bin" >> "$HOME/.zshrc"
+      echo "Added Lesma to PATH in .zshrc, please run 'source ~/.zshrc' to reload shell"
+    fi
+
+    if [[ ! -f "$HOME/.zshrc" && ! -f "$HOME/.bashrc" ]]; then
+      echo "No suitable shell found, please add $HOME/.lesma to PATH"
+    fi
+  fi
+fi
