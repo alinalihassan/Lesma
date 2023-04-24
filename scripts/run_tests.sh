fbc0f1297489302074196aace24995710799b30a
Alin Ali Hassan <hassanalinali@gmail.com>
2023-04-24 23:15:20 +0200

    chore: modified test script to pass path to executable

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -8,22 +8,24 @@ success_count=0
 test_compiler() {
   local file="$1"
   local mode="$2"
-  "$SCRIPT_DIR"/../build/lesma "${mode}" "${file}" &> /dev/null
+  local compiler_path="$3"
+  "${compiler_path}" "${mode}" "${file}" &> /dev/null
   return $?
 }
 
 test_case() {
   local file="$1"
   local expected_to_fail="$2"
+  local compiler_path="$3"
   name=$(basename -s .les "${file}")
   printf "Testing %s\n" "${name}"
 
   test_expected_ret_value=0
 
-  test_compiler "${file}" "run"
+  test_compiler "${file}" "run" "${compiler_path}"
   test_jit_ret_value=$?
 
-  test_compiler "${file}" "compile"
+  test_compiler "${file}" "compile" "${compiler_path}"
   test_aot_ret_value=$?
 
   if [ "$test_jit_ret_value" -ne "$test_expected_ret_value" ] && [ "$expected_to_fail" -eq 0 ]; then
@@ -41,14 +43,19 @@ test_case() {
   fi
 }
 
+compiler_path="${SCRIPT_DIR}/../build/lesma"
+if [ $# -gt 0 ]; then
+  compiler_path="$1"
+fi
+
 # Test success cases
 for file in "$SCRIPT_DIR"/../tests/lesma/success/*.les; do
-  test_case "${file}" 0
+  test_case "${file}" 0 "${compiler_path}"
 done
 
 # Test failure cases
 for file in "$SCRIPT_DIR"/../tests/lesma/failure/*.les; do
-  test_case "${file}" 1
+  test_case "${file}" 1 "${compiler_path}"
 done
 
 printf "Tests:\n"
